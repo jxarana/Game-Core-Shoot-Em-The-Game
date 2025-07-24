388f4afccebe7aac757537cf5422d5a3a7eda7b0
Connor Kulaga <Connorkulaga03@gmail.com>
2025-07-23 20:31:51 -0400

    Fixed broken stuff Pt1

--- Team Project/FPS - 2507/Assets/Scripts/playerController.cs
+++ Team Project/FPS - 2507/Assets/Scripts/playerController.cs
@@ -8,6 +8,9 @@ public class playerController : MonoBehaviour, IDamage, IInventorySystem, ICanGr
 {
     [SerializeField] CharacterController controller;
     [SerializeField] LayerMask ignoreLayer;
+    [SerializeField] Transform orientation;
+    [SerializeField] LayerMask wallLayer;
+    [SerializeField] Animator animator;
     [Header("General Stats")]
     [SerializeField] int HPOrig;
     [SerializeField] int speed;
@@ -16,12 +19,22 @@ public class playerController : MonoBehaviour, IDamage, IInventorySystem, ICanGr
     [SerializeField] int jumpMax;
     [SerializeField] int gravity;
     [SerializeField] int dashMax;
+    [SerializeField] int deathDepth; // Set the height that the player can fall to before dieing
     [SerializeField] int dashCd;
-    [SerializeField] Transform camPivot;
+    //[SerializeField] Transform camPivot;
     [SerializeField] float mouseSensitivity = 3f;
     [SerializeField] public playerStats upgradeableStats;
     [SerializeField] GameObject gunModel;
     [SerializeField] GameObject keyModel;
+
+    [SerializeField] float climbSpeed;
+    [SerializeField] float maxClimbTime;
+    private float climbTimer;
+
+    [SerializeField] float detectLength;
+    [SerializeField] float sphereCastRadius;
+    [SerializeField] float maxWallLookAngle;
+    private float wallLookAngle;
 
     public int goldCount;
     public int upgradePoints;
@@ -46,31 +59,37 @@ public class playerController : MonoBehaviour, IDamage, IInventorySystem, ICanGr
     bool slaming;
     bool canSlam;
 
-    int dashDuration = 1;
-
-    [SerializeField] public float grappleStaminaCost;
     [SerializeField] public float staminaOrig;
+    public float stamina;
+    [SerializeField] float staminaRegenRate;
+    [SerializeField] float sprintStaminaCost;
+    [SerializeField] float mantleStaminaCost;
+    [SerializeField] float climbStaminaCost;
+    public float grappleStaminaCost;
+    bool isSprinting;
+    bool canUseStamina;
 
     [SerializeField] float mantleCheckDist = 1f;
     [SerializeField] float mantleHeight = 1.5f;
     [SerializeField] float mantleDuration = 0.3f;
     [SerializeField] LayerMask mantleLayer;
 
+    [SerializeField] AudioClip gunClip;
+    [SerializeField] AudioClip deathClip;
+
     bool isMantling = false;
     Vector3 mantleStartPos;
     Vector3 mantleEndPos;
     float mantleTimer;
 
-    public float stamina;
     int dashCount;
     public float dashSpeed;
-
+    public float dashDuration;
+    public float dashCooldown;
     float dashCooldownTimer;
     bool isDashing;
-
+    float dashTimeLeft;
     private Vector3 dashDirection;
-
-
 
     Vector3 moveDir;
     Vector3 playerVel;
@@ -82,10 +101,9 @@ public class playerController : MonoBehaviour, IDamage, IInventorySystem, ICanGr
 
     float shootTimer;
     float xRotation = 0f;
-
-
-
-
+    bool hasSlamunlocked = false;
+    bool hasDashUnlocked = false;
+    bool hasGrappleUnlocked = false;
     int magCurrent;
     int currentAmmo;
 
@@ -94,12 +112,20 @@ public class playerController : MonoBehaviour, IDamage, IInventorySystem, ICanGr
 
     public bool isGrappling;
 
+    private bool isClimbing;
+    private bool isWallFront;
+    private RaycastHit wallHit;
+ 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         HP = HPOrig;
         speedOrig = speed + upgradeableStats.speed;
+        canUseStamina = true;
+        isSprinting = false;
+        stamina = staminaOrig;
         isGrappling = false;
+        isMantling = false;
         magCurrent = magMax;
         currentAmmo = maxAmmo;
         dashCount = dashMax + upgradeableStats.maxDashes;
@@ -114,6 +140,19 @@ public class playerController : MonoBehaviour, IDamage, IInventorySystem, ICanGr
     // Update is called once per frame
     void Update()
     {
+        // Regenerate stamina only if not doing stamina-draining actions
+        if (!isSprinting && !isGrappling && !isClimbing && !isMantling && stamina < staminaOrig)
+        {
+            stamina += staminaRegenRate * Time.deltaTime;
+            stamina = Mathf.Clamp(stamina, 0, staminaOrig);
+            updatePlayerUI();
+        }
+
+        if (gameManager.instance != null && gameManager.instance.isPaused)
+        {
+            return;
+        }
+
         if (isMantling)
         {
             MantleMove();
@@ -128,12 +167,49 @@ public class playerController : MonoBehaviour, IDamage, IInventorySystem, ICanGr
         {
             TryMantle();
         }
-        handleCamera();
+        // handleCamera();
+
+        wallCheck();
+        stateMachine();
 
     }
 
+    private void stateMachine()
+    {
+        if (isWallFront && Input.GetKey(KeyCode.C) && wallLookAngle < maxWallLookAngle)
+        {
+            if (!isClimbing && climbTimer > 0 && stamina > 0)
+            {
+                startClimbing();
+            }
+
+            if (climbTimer > 0)
+            {
+                climbTimer -= Time.deltaTime;
+            }
+            if (climbTimer < 0)
+            {
+                stopClimbing();
+            }
+        }
+        else
+        {
+            if (isClimbing)
+            {
+                stopClimbing();
+            }
+        }
+    }
+
+
     void movement()
     {
+        // Player instantly dies if he falls to a certain depth on the map
+        if (gameObject.transform.position.y <= deathDepth)
+        {
+            gameManager.instance.youLose();
+        }
+
         if (isMantling)
         {
             return;
@@ -154,11 +230,18 @@ public class playerController : MonoBehaviour, IDamage, IInventorySystem, ICanGr
         {
             playerVel = Vector3.zero;
             jumpCount = 0;
+            dashCount = 0;
 
         }
 
         moveDir = (Input.GetAxis("Horizontal") * transform.right) + (Input.GetAxis("Vertical") * transform.forward);
         controller.Move(moveDir * speed * Time.deltaTime);
+
+        float horizontalMovement = Input.GetAxis("Horizontal");
+        float verticalMovement = Input.GetAxis("Vertical");
+        animator.SetFloat("Horizontal", horizontalMovement);
+        animator.SetFloat("Vertical", verticalMovement);
+
 
         jump();
 
@@ -202,8 +285,11 @@ public class playerController : MonoBehaviour, IDamage, IInventorySystem, ICanGr
                 updatePlayerUI();
             }
         }
-
         selectGun();
+        if (isClimbing)
+        {
+            climbingMovement();
+        }
     }
 
     void jump()
@@ -213,17 +299,46 @@ public class playerController : MonoBehaviour, IDamage, IInventorySystem, ICanGr
             playerVel += transform.up * jumpVel;
             jumpCount++;
         }
+
+        if (isClimbing)
+        {
+            climbingMovement();
+        }
     }
 
     void sprint()
     {
-        if (Input.GetButtonDown("Sprint"))
+        if (Input.GetButton("Sprint") && controller.isGrounded && moveDir.magnitude > 0.1f && stamina > 0)
         {
-            speed *= sprintMod;
+            if (!isSprinting)
+            {
+                isSprinting = true;
+                speed *= sprintMod;
+            }
+
+            //reduce stamina by the cost and clamp so it doesn't go outside of the stamina range
+            stamina -= sprintStaminaCost * Time.deltaTime;
+            stamina = Mathf.Clamp(stamina, 0f, staminaOrig);
+            updatePlayerUI();
+
+            if (stamina <= 0.1f)
+            {
+                stamina = 0;
+                StopSprinting();
+            }
         }
-        else if (Input.GetButtonUp("Sprint"))
+        else if(Input.GetButtonUp("Sprint"))
         {
-            speed /= sprintMod;
+            StopSprinting();
+        }
+    }
+
+    void StopSprinting()
+    {
+        if (isSprinting)
+        {
+            speed = speedOrig;
+            isSprinting = false;
         }
     }
 
@@ -237,10 +352,16 @@ public class playerController : MonoBehaviour, IDamage, IInventorySystem, ICanGr
             Vector3 ledgeCheckOrigin = wallHit.point + Vector3.up * mantleHeight;
             if (Physics.Raycast(ledgeCheckOrigin, Vector3.down, out RaycastHit topHit, mantleHeight, mantleLayer))
             {
-                isMantling = true;
-                mantleTimer = 0f;
-                mantleStartPos = transform.position;
-                mantleEndPos = new Vector3(topHit.point.x, topHit.point.y + 0.1f, topHit.point.z);
+                if (stamina >= mantleStaminaCost)
+                {
+                    stamina -= mantleStaminaCost;
+                    updatePlayerUI();
+
+                    isMantling = true;
+                    mantleTimer = 0f;
+                    mantleStartPos = transform.position;
+                    mantleEndPos = new Vector3(topHit.point.x, topHit.point.y + 0.1f, topHit.point.z);
+                }
             }
         }
     }
@@ -298,6 +419,7 @@ public class playerController : MonoBehaviour, IDamage, IInventorySystem, ICanGr
     public void updatePlayerUI()
     {
         gameManager.instance.playerHPBar.fillAmount = (float)HP / HPOrig;
+        gameManager.instance.playerStaminaBar.fillAmount = (float)stamina / staminaOrig;
         gameManager.instance.ammoBar.fillAmount = (float)magCurrent / magMax;
         gameManager.instance.inMagCount.text = magCurrent.ToString();
         gameManager.instance.currAmmoCount.text = currentAmmo.ToString();
@@ -333,9 +455,19 @@ public class playerController : MonoBehaviour, IDamage, IInventorySystem, ICanGr
         HP = Mathf.Min(HP + ammount, HPOrig);
         updatePlayerUI();
     }
+    public void dashUnlock()
+    {
+        hasDashUnlocked = true;
+    }
+    public void grappleUnlock()
+    {
+        hasGrappleUnlocked = true;
+    }
 
-
-
+    public void slamUnlock()
+    {
+        hasSlamunlocked = true;
+    }
     public void dashCountUp()
     {
         dashMax += 1;
@@ -351,19 +483,72 @@ public class playerController : MonoBehaviour, IDamage, IInventorySystem, ICanGr
         speed += 1;
         speedOrig = speed;
     }
-
-
-    void handleCamera()
+    public bool dashReturn()
     {
-        if (Time.timeScale <= 0f) return;
-        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity;
-        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity;
+        return hasDashUnlocked;
+    }
 
-        transform.Rotate(Vector3.up * mouseX);
+    public bool grappleReturn()
+    {
+        return hasGrappleUnlocked;
+    }
 
-        xRotation -= mouseY;
-        xRotation = Mathf.Clamp(xRotation, -60f, 60f);
-        camPivot.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+    public bool slamReturn()
+    {
+
+        return hasSlamunlocked;
+    }
+
+
+    //void handleCamera()
+    //{
+    //    if (Time.timeScale <= 0f) return;
+    //    float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity;
+    //    float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity;
+
+    //    transform.Rotate(Vector3.up * mouseX);
+
+    //    xRotation -= mouseY;
+    //    xRotation = Mathf.Clamp(xRotation, -60f, 60f);
+    //    camPivot.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+    //}
+
+    private void wallCheck()
+    {
+        isWallFront = Physics.SphereCast(transform.position, sphereCastRadius, orientation.forward, out wallHit, detectLength, wallLayer);
+        wallLookAngle = Vector3.Angle(orientation.forward, -wallHit.normal);
+
+        if (controller.isGrounded)
+        {
+            climbTimer = maxClimbTime;
+        }
+    }
+    private void climbingMovement()
+    {
+        if (stamina > 0)
+        {
+            stamina -= climbStaminaCost * Time.deltaTime;
+            stamina = Mathf.Clamp(stamina, 0, staminaOrig);
+            updatePlayerUI();
+
+            //Move upward while stamina is still available
+            playerVel = new Vector3(playerVel.y, climbSpeed, playerVel.z);
+        }
+        else
+        {
+            //Stop climbing if stamina runs out
+            stopClimbing();
+        }
+    }
+
+    private void startClimbing()
+    {
+        isClimbing = true;
+    }
+
+    private void stopClimbing()
+    {
+        isClimbing = false;
     }
 
     IEnumerator Dash()
