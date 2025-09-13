0353011452388ad2459a957b31a4b6a80636e57b
Darrion Samuel <darrionsamuels5@gmail.com>
2025-09-13 01:27:31 -0500

    Commit

--- Team Project/FPS - 2507/Assets/Scripts/IInventorySystem.cs
+++ Team Project/FPS - 2507/Assets/Scripts/IInventorySystem.cs
@@ -1,8 +1,3 @@
-using UnityEngine;
-
-public interface IInventorySystem
-{
-    public void getItemPickUp(itemPickUp item);
- 
-    public void getGunStats(gunStats gun);
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:3d39265dbe68bffe20e203d0c0f75d1434afde5adee60b72b2f46b10feea3c72
+size 150
