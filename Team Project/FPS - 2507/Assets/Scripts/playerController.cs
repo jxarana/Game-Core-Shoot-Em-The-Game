d64589aeef581cbac88180b70c202784f7926dbf
jxarana <JXArana@student.fullsail.edu>
2025-09-13 02:54:01 -0400

    Added death physics to player model

--- Team Project/FPS - 2507/Assets/Scripts/playerController.cs
+++ Team Project/FPS - 2507/Assets/Scripts/playerController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c6ee15d32b06ff204a0b5faa6d57ff72d9306463ac8ffc1849b852e96971048d
-size 23336
+oid sha256:ade65df82ad6de973518a7b1c7f65f4445de693d56dfaba3cbb10254aedca221
+size 24241
