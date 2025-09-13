b4b93bf3b214de78a3cc4c4decf664c5f62037e0
Connor Kulaga <Connorkulaga03@gmail.com>
2025-09-12 22:06:43 -0400

    Fix again what I broke

--- Team Project/FPS - 2507/Assets/Scripts/playerController.cs
+++ Team Project/FPS - 2507/Assets/Scripts/playerController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9fe0a489b14c9d055b58b272d089f531d49a377d4754e5ebacc54bb90eeaafe9
-size 23006
+oid sha256:b75a86d49676fd504ab7ffd094f9611ea07f32d5de6fdd49c43d74aa29e8f56b
+size 23070
