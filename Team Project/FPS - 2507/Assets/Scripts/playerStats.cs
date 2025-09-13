31c09ef0c4ad8bcbf8c268e597b01afa48365922
jxarana <JXArana@student.fullsail.edu>
2025-09-13 01:30:27 -0400

    comitting

--- Team Project/FPS - 2507/Assets/Scripts/playerStats.cs
+++ Team Project/FPS - 2507/Assets/Scripts/playerStats.cs
@@ -1,18 +1,3 @@
-using UnityEngine;
-
-[CreateAssetMenu]
-public class playerStats : ScriptableObject
-{
-    public int speed;
-    public int maxDashes;
-    public int maxJumps;
-    
-    public int dmgIncreased;
-    
-    
-
- 
-
-
-
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:3c6b7407762030037771f0112cc28e01fde83204acd6ca2f1738752e150fafa1
+size 209
