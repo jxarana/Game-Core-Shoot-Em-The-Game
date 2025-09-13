0b70a125726c63c942d0d67a39aac71224744f1d
Connor Kulaga <Connorkulaga03@gmail.com>
2025-09-13 18:01:24 -0400

    Climbing animation

--- Team Project/FPS - 2507/Assets/Scripts/playerController.cs
+++ Team Project/FPS - 2507/Assets/Scripts/playerController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1fa79a6196116453820c558546af1f6c3eedc0256a5769a7d450ec7b3aecff45
-size 24883
+oid sha256:22d2af79084a723a51911c13e4a0f0f12dc9502e22cd442e1be0d037b0abf8cf
+size 24978
