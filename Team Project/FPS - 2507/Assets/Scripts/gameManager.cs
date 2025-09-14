9e1dbd349da1837542147335a56ba1db090c8eeb
Connor Kulaga <Connorkulaga03@gmail.com>
2025-09-13 20:26:55 -0400

    Revert "Merge branch 'Safety' into Dev-Connor-Kulaga"
    This reverts commit 0890df4cd2d0d998636aec8f81e45b7188001581, reversing
    changes made to cc5a69421acad2501f0bfb9b6fa4b896b9998f12.

--- Team Project/FPS - 2507/Assets/Scripts/gameManager.cs
+++ Team Project/FPS - 2507/Assets/Scripts/gameManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bcbed9796e909444f00a71dfc89a908d67794f1c56b6ab5263e519ed1c3d2e84
-size 267
+oid sha256:f0154004f6456580afddb793e7bb668fa0f3254fc7a95bf6df776e661047caa0
+size 9956
