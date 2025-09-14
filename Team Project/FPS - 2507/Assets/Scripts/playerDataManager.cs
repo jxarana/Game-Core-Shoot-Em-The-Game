ec2e04f422b568a650fa5552fe68f6f7a3e1a97d
Connor Kulaga <Connorkulaga03@gmail.com>
2025-09-13 21:41:14 -0400

    fix

--- Team Project/FPS - 2507/Assets/Scripts/playerDataManager.cs
+++ Team Project/FPS - 2507/Assets/Scripts/playerDataManager.cs
@@ -1,3 +1,16 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:31a6272024910c0154953eabc88b12d6b59ce91058f112699b83a48b6525b557
-size 288
+using UnityEngine;
+
+public class playerDataManager : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
